451ae409fca03c1d5b16d487f76529d5140cbe74
wdecoster <decosterwouter@gmail.com>
2018-10-27 03:43:31 +0200

    functional version

--- fast5purge/fast5purge.py
+++ fast5purge/fast5purge.py
@@ -1,3 +1,41 @@
 from ont_fast5_api.fast5_file import Fast5File
-f = Fast5File("test.fast5", "r+")
- f.set_tracking_id({}, clear=True)
+from argparse import ArgumentParser
+import sys
+import glob
+
+
+def main():
+    args = get_args()
+    if args.file:
+        purge(args.file)
+    else:
+        for filename in glob.iglob(args.dir + '**/*.txt', recursive=args.recursive):
+            purge(filename)
+
+
+def purge(fn):
+    f = Fast5File(fn, "r+")
+    f.set_tracking_id({}, clear=True)
+    f.add_context_tags({}, clear=True)
+    f.close()
+
+
+def get_args():
+    parser = ArgumentParser(description="Remove all sensible content from a fast5")
+    parser.add_argument("-d", "--dir", help="directory in which fast5 files have to be purged")
+    parser.add_argument("-f", "--file", help="single fast5 file to purge")
+    parser.add_argument("-r", "--recursive",
+                        help="recursively go through directory",
+                        action="store_true")
+    args = parser.parse_args()
+    if not args.dir and not args.file:
+        sys.exit("ARGUMENT ERROR: Exactly one of --dir or --file is required")
+    if args.dir and args.file:
+        sys.exit("ARGUMENT ERROR: Exactly one of --dir or --file is required")
+    if args.recursive and args.file:
+        sys.exit("ARGUMENT ERROR: -r/--recursive is only applicable when selecting a directory")
+    return args
+
+
+if __name__ == '__main__':
+    main()
